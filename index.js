ccab5a0322cb150794a1ca6540ce850392c9fbdc
xaviguasch <xaviguasch@gmail.com>
2022-06-02 20:29:40 +0200

    added js elements to the ejs file from the server.js file

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
+app.get('/r/:subreddit', (req, res) => {
+  const { subreddit } = req.params
+
+  res.render('subreddit', { subreddit })
+})
+
+app.get('/rand', (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1
+  res.render('random', { num })
+})
+
 app.listen(3000, () => {
   console.log('LISTENING ON PORT 3000!')
 })
